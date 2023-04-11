c1e05055e695b13abfe1a50f5a36b99327521df0
swatiitobuz <swati@itobuz.com>
2023-04-11 13:30:53 +0530

    commited 11.04.2023

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -6,6 +6,9 @@ let tittle = document.querySelectorAll(".title");
 let details = document.querySelectorAll(".details");
 let save = document.getElementById("save-button");
 
+//display
+
+
 async function display() {
   const response = await fetch("http://localhost:5000/read", {
     method: "GET",
@@ -17,16 +20,44 @@ async function display() {
       console.log(error);
     });
   for (let i = 0; i < response.data.length; i++) {
-    document.getElementById("display").innerHTML += `<div class = "notes-data">
-  <h1 class="tittle">${response.data[i].headline}</h1>
-  <p class="details">${response.data[i].description}</p>
-  <input id="edit-button" type="button" value="edit" onclick="editNote()" />
-  <input id="delete-button" type="button" value="delete" onclick="deleteNote('${response.data[i]._id}')" />
-  <input id="save-button" type="button" style="display:none;" value="save" onclick="saveData('${response.data[i]._id}')" />
+    document.getElementById(
+      "display"
+    ).innerHTML += `<div class="notes-data" onclick="editNote(this)">
+    <img
+      class="expand"
+      src="./images/icons8-enlarge-32.png"
+      alt="expand"
+      onclick="expand()"
+    />
+    <h1 class="tittle">${response.data[i].headline}</h1>
+    <p class="details">${response.data[i].description}</p>
+    <input class="edit-button" type="button" value="edit" />
+    <input
+      class="delete-button"
+      type="button"
+      value="delete"
+      onclick="showConfirmation()"
+    />
+    <div class="text-right confirm">
+      <p class="details">Are you sure?</p>
+      <button class="cancel" onclick="cancelNote()">Cancel</button>
+      <button class="ok" onclick="deleteNote('${response.data[i]._id}')">
+        Ok
+      </button>
+    </div>
+    <input
+      class="save-button"
+      type="button"
+      style="display: none"
+      value="save"
+      onclick="saveData('${response.data[i]._id}')"
+    />
   </div>`;
   }
 }
 display();
+
+//addNote
 
 async function addNote() {
   if (headlineText.value.trim().length === 0) {
@@ -55,37 +86,45 @@ async function addNote() {
 
 //edit
 
-async function editNote() {
-  document.querySelector("#edit-button").style.display = "none";
-  document.querySelector("#save-button").style.display = "block";
-  document.querySelector("#submit-button").style.display = "none";
+async function editNote(e) {
+  console.log(e);
+  e.querySelector(".edit-button").style.display = "none";
+  e.querySelector(".save-button").style.display = "block";
+  e.querySelector(".delete-button").style.display = "block";
+  headlineText.value = e.querySelector("h1").innerText;
+  contentText.value = e.querySelector("p").innerText;
+
+  displayInput();
 }
 
+//save data
+
 async function saveData(id) {
-  console.log(id);
-  if(headlineText.value.trim().length === 0 || contentText.value.trim().length === 0){
+  if (
+    headlineText.value.trim().length === 0 ||
+    contentText.value.trim().length === 0
+  ) {
     alert("please edit properly");
-  }
-  else{
+  } else {
     let editApi = "http://localhost:5000/" + id;
-  console.log(editApi);
-  await fetch(editApi, {
-    method: "PATCH",
-    body: JSON.stringify({
-      headline: headlineText.value,
-      description: contentText.value,
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-  headlineText.value = "";
-  contentText.value = "";
-  displays.innerHTML = "";
-  display();
-  document.querySelector("#submit-button").style.display = "block";
+    console.log(editApi);
+    await fetch(editApi, {
+      method: "PATCH",
+      body: JSON.stringify({
+        headline: headlineText.value,
+        description: contentText.value,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    headlineText.value = "";
+    contentText.value = "";
+    displays.innerHTML = "";
+    display();
+    document.querySelector("#submit-button").style.display = "block";
   }
 }
 
@@ -97,5 +136,43 @@ async function deleteNote(id) {
     method: "DELETE",
   });
   displays.innerHTML = "";
+  headlineText.value = "";
+  contentText.value = "";
   display();
+
 }
+
+//inputBox
+
+function displayInput() {
+  let inputDetails = document.querySelector(".notes");
+  if (inputDetails.classList.contains("toggle-class")) {
+    inputDetails.classList.remove("toggle-class");
+  } else {
+    inputDetails.classList.add("toggle-class");
+  }
+}
+
+//confirmation
+
+
+function showConfirmation() {
+  displayInput();
+  document.querySelector(".notes").classList.add("toggle-class");
+  document.querySelector(".text-right").style.display = "block";
+}
+
+
+//expand
+
+function expand(){
+
+}
+
+//cancel
+
+function cancelNote(){
+  document.querySelector(".text-right").style.display = "none";
+}
+
+//pop up
